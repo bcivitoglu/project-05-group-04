3244b81b0fb6d8f78a464fae3835da8830b3cb7d
TimKuehn <T.Kuehn@stud.uni-heidelberg.de>
2019-06-03 18:12:55 +0200

    Updaten nested for loop

--- R script (Tim file).R
+++ R script (Tim file).R
@@ -27,15 +27,15 @@ threshhold1 <- 10
 threshhold2 <- quantile(cov_genes_mean, probs = seq(0.95,0.95,0.05))
 
 
-##Actual nested for loop -> No error anymore, but still doesn´t set the values over and under threshhold to NA
+##Nested for loop 
 
-  for(i in ncol(cov_genes)){
-    for(j in nrow((cov_genes))){
-      if(cov_genes[i,j] <= threshhold1){
-        cov_genes[i,j] <- NA
-      }
-      if(cov_genes[i,j] >= threshhold2){
-        cov_genees[i,j] <- NA
-      }
+for(i in 1:ncol(cov_genes)){
+  for(j in 1:nrow(cov_genes)){
+    if(isTRUE(cov_genes[i,j] <= threshhold1)){
+      cov_genes[i,j] <- NA
+    } 
+    if(isTRUE(cov_genes[i,j] >= threshhold2)){
+      cov_genes[i,j] <- NA
     }
   }
+}
